3856af1d8bdda91c7e2929e562e76b6d65826327
akicho8 <akicho8@gmail.com>
2013-07-12 09:20:58 +0900

    おまけのWEBインタフェースのテストも書いた

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -3,38 +3,66 @@
 require "spec_helper"
 
 describe ArticlesController do
-  # context do
-  #   it do
-  #     get :index
-  #     response.should be_success
-  #   end
-  # end
+  before do
+    @article = FactoryGirl.create(:article, :tag_list => "t1")
+  end
 
-  context "Emacs対応" do
-    it "書き込みできる" do
-      post :text_post, :content => "
+  it do
+    get :index
+    response.should be_success
+  end
+
+  it do
+    get :show, :id => @article.id
+    response.should be_success
+  end
+
+  it do
+    get :new
+    response.should be_success
+  end
+
+  it do
+    post :create, :article => {:title => "(title)", :tag_list => "(tag_list)", :body => "(body)"}
+    response.should be_redirect
+  end
+
+  it do
+    get :edit, :id => @article.id
+    response.should be_success
+  end
+
+  it do
+    put :update, :id => @article.id, :article => {:title => "(title)", :tag_list => "(tag_list)", :body => "(body)"}
+    response.should be_redirect
+  end
+
+  it do
+    delete :destroy, :id => @article.id
+    response.should be_redirect
+  end
+
+  it do
+    get :index, :query => "t1", :format => "txt"
+    response.should be_success
+    response.body == <<-EOT
+--------------------------------------------------------------------------------
+Id: #{@article.id}
+Title: #{@article.title}
+Tag: t1
+--text follows this line--
+#{@article.body}
+--------------------------------------------------------------------------------
+EOT
+  end
+
+  it do
+    post :text_post, :content => "
 Title: タイトル
 Tag: a b c
 --text follows this line--
 本文
 "
-      response.should be_success
-    end
-
-    it "読み出せる" do
-      article = FactoryGirl.create(:article, :tag_list => "t1")
-      get :index, :query => "t1", :format => "txt"
-      response.should be_success
-      assigns(:articles).should == [article]
-      response.body == <<-EOT
---------------------------------------------------------------------------------
-Id: #{article.id}
-Title: #{article.title}
-Tag: t1
---text follows this line--
-#{article.body}
---------------------------------------------------------------------------------
-EOT
-    end
+    response.should be_success
   end
 end
