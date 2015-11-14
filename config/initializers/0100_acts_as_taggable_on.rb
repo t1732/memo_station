c6f3b09ff7cf50ae4d24bbbfb014e3542e9d14b4
akicho8 <akicho8@gmail.com>
2015-11-15 01:05:51 +0900

    Revert "ActsAsTaggableOn >= 4.0 のために Parser を独自定義"
    This reverts commit b02c2069a7e6653c686f739d99eda698d845060d.
    
    split(/\s+/) とするだけではダメで、
    tag_list.to_s のときにカンマ区切りになってしまう対策が必要。
    しかし、ActsAsTaggableOn.delimiter をそのまま使うようになっていて、
    現状のバージョンだと 4.0 用のコードに移行できない。

--- config/initializers/0100_acts_as_taggable_on.rb
+++ config/initializers/0100_acts_as_taggable_on.rb
@@ -1,14 +1,4 @@
-module ActsAsTaggableOn
-  class WhiteSpaceSplitParser < GenericParser
-    def parse
-      ActsAsTaggableOn::TagList.new.tap do |tag_list|
-        tag_list.add @tag_list.split(/\s+/)
-      end
-    end
-  end
-end
-
 ActsAsTaggableOn.setup do |config|
-  config.default_parser = ActsAsTaggableOn::WhiteSpaceSplitParser
+  config.delimiter = " "
   config.remove_unused_tags = true
 end
