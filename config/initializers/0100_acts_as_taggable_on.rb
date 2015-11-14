32ff0c82d411015ea228f1fdda536317f592a68c
akicho8 <akicho8@gmail.com>
2015-11-15 01:05:51 +0900

    ActsAsTaggableOn >= 4.0 のために Parser を独自定義

--- config/initializers/0100_acts_as_taggable_on.rb
+++ config/initializers/0100_acts_as_taggable_on.rb
@@ -1,4 +1,14 @@
+module ActsAsTaggableOn
+  class WhiteSpaceSplitParser < GenericParser
+    def parse
+      ActsAsTaggableOn::TagList.new.tap do |tag_list|
+        tag_list.add @tag_list.split(/\s+/)
+      end
+    end
+  end
+end
+
 ActsAsTaggableOn.setup do |config|
-  config.delimiter = " "
+  config.default_parser = ActsAsTaggableOn::WhiteSpaceSplitParser
   config.remove_unused_tags = true
 end
